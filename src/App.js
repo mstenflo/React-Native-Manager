afa2d09a9abaac3ff001afdc62676e58c8029a50
Martin Stenflo <stenflo@gmail.com>
2020-08-13 11:24:02 -0700

    add firebase to project

--- src/App.js
+++ src/App.js
@@ -2,11 +2,33 @@ import React, { Component } from 'react';
 import { View, Text } from 'react-native';
 import { Provider } from 'react-redux';
 import { createStore } from 'redux';
+import firebase from 'firebase';
+
+import reducers from './reducers';
 
 class App extends Component {
+
+  UNSAFE_componentWillMount() {
+    const firebaseConfig = {
+      apiKey: 'AIzaSyDFmRBtIP7xIjfhlR1uMXHh4ONcpNN6MWg',
+      authDomain: 'manager-e20a8.firebaseapp.com',
+      databaseURL: 'https://manager-e20a8.firebaseio.com',
+      projectId: 'manager-e20a8',
+      storageBucket: 'manager-e20a8.appspot.com',
+      messagingSenderId: '865101317394',
+      appId: '1:865101317394:web:79c55ded9c779b46721fcb',
+      measurementId: 'G-W7M0Y9QTPL'
+    };
+
+    if (!firebase.apps.length) {
+      firebase.initializeApp(firebaseConfig);
+      // firebase.analytics();
+    }
+  }
+
   render() {
     return (
-      <Provider store={createStore()}>
+      <Provider store={createStore(reducers)}>
         <View>
           <Text>
             Hello!
