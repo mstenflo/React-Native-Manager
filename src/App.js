1fb22764ec4b4a95b0c10cbd248b7c6dc6ade7cf
Martin Stenflo <stenflo@gmail.com>
2020-08-13 17:02:01 -0700

    add thunk middleware

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
 import { Provider } from 'react-redux';
-import { createStore } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
 import firebase from 'firebase';
+import ReduxThunk from 'redux-thunk';
 
 import reducers from './reducers';
 import LoginForm from './components/LoginForm';
@@ -23,18 +23,16 @@ class App extends Component {
 
     if (!firebase.apps.length) {
       firebase.initializeApp(firebaseConfig);
-      // firebase.analytics();
+      firebase.analytics();
     }
   }
 
   render() {
+    const store = createStore(reducers, {}, applyMiddleware(ReduxThunk))
+
     return (
-      <Provider store={createStore(reducers)}>
-        <LoginForm>
-          <Text>
-            Hello!
-          </Text>
-        </LoginForm>
+      <Provider store={store}>
+        <LoginForm />
       </Provider>
     );
   }
