be8e481e9d8932c523b1b5f9245ce9c4ed06f394
Martin Stenflo <stenflo@gmail.com>
2020-08-17 15:09:13 -0700

    employee create form

--- src/components/EmployeeList.js
+++ src/components/EmployeeList.js
@@ -1,19 +1,34 @@
+import _ from 'lodash';
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
-
+import { connect } from 'react-redux';
+import { FlatList } from 'react-native';
+import { employeesFetch } from '../actions';
+import ListItem from './ListItem'
+ 
 class EmployeeList extends Component {
-  render() {
-    return (
-      <View>
-        <Text>Employee List</Text>
-        <Text>Employee List</Text>
-        <Text>Employee List</Text>
-        <Text>Employee List</Text>
-        <Text>Employee List</Text>
-        <Text>Employee List</Text>
-      </View>
-    );
-  }
+	componentWillMount() {
+		this.props.employeesFetch();
+	}
+	renderItem(employee) {
+		return <ListItem employee={employee} />;
+	}
+	render() {
+		return (
+			<FlatList
+				data={this.props.employees}
+				renderItem={this.renderItem.bind(this)}
+				keyExtractor={(employee, index) => index.toString()}
+			/>
+		);
+	}
 }
-
-export default EmployeeList;
+ 
+const mapStateToProps = (state) => {
+    const employees = _.map(state.employees, (val, uid) => {
+        return { ...val, uid };
+    });
+ 
+    return { employees }
+};
+ 
+export default connect(mapStateToProps, { employeesFetch })(EmployeeList);
